55c0e5ca788f6ef4716bd510f22923befaa86c44
bingyu <bingyu.zhou@siemens.com>
2018-12-10 13:57:35 +0100

    let the policy run

--- spinup/utils/test_policy.py
+++ spinup/utils/test_policy.py
@@ -6,18 +6,22 @@ import tensorflow as tf
 from spinup import EpochLogger
 from spinup.utils.logx import restore_tf_graph
 
+
 def load_policy(fpath, itr='last', deterministic=False):
 
     # handle which epoch to load from
-    if itr=='last':
-        saves = [int(x[11:]) for x in os.listdir(fpath) if 'simple_save' in x and len(x)>11]
-        itr = '%d'%max(saves) if len(saves) > 0 else ''
+    if itr == 'last':
+        saves = [
+            int(x[11:]) for x in os.listdir(fpath)
+            if 'simple_save' in x and len(x) > 11
+        ]
+        itr = '%d' % max(saves) if len(saves) > 0 else ''
     else:
-        itr = '%d'%itr
+        itr = '%d' % itr
 
     # load the things!
     sess = tf.Session()
-    model = restore_tf_graph(sess, osp.join(fpath, 'simple_save'+itr))
+    model = restore_tf_graph(sess, osp.join(fpath, 'simple_save' + itr))
 
     # get the correct op for executing actions
     if deterministic and 'mu' in model.keys():
@@ -29,12 +33,12 @@ def load_policy(fpath, itr='last', deterministic=False):
         action_op = model['pi']
 
     # make function for producing an action given a single state
-    get_action = lambda x : sess.run(action_op, feed_dict={model['x']: x[None,:]})[0]
+    get_action = lambda x : sess.run(action_op, feed_dict={model['x']: x[None,:]})
 
     # try to load environment from save
     # (sometimes this will fail because the environment could not be pickled)
     try:
-        state = joblib.load(osp.join(fpath, 'vars'+itr+'.pkl'))
+        state = joblib.load(osp.join(fpath, 'vars' + itr + '.pkl'))
         env = state['env']
     except:
         env = None
@@ -42,7 +46,8 @@ def load_policy(fpath, itr='last', deterministic=False):
     return env, get_action
 
 
-def run_policy(env, get_action, max_ep_len=None, num_episodes=100, render=True):
+def run_policy(env, get_action, max_ep_len=None, num_episodes=100,
+               render=True):
 
     assert env is not None, \
         "Environment not found!\n\n It looks like the environment wasn't saved, " + \
@@ -63,7 +68,7 @@ def run_policy(env, get_action, max_ep_len=None, num_episodes=100, render=True):
 
         if d or (ep_len == max_ep_len):
             logger.store(EpRet=ep_ret, EpLen=ep_len)
-            print('Episode %d \t EpRet %.3f \t EpLen %d'%(n, ep_ret, ep_len))
+            print('Episode %d \t EpRet %.3f \t EpLen %d' % (n, ep_ret, ep_len))
             o, r, d, ep_ret, ep_len = env.reset(), 0, False, 0, 0
             n += 1
 
@@ -82,7 +87,6 @@ if __name__ == '__main__':
     parser.add_argument('--itr', '-i', type=int, default=-1)
     parser.add_argument('--deterministic', '-d', action='store_true')
     args = parser.parse_args()
-    env, get_action = load_policy(args.fpath, 
-                                  args.itr if args.itr >=0 else 'last',
-                                  args.deterministic)
-    run_policy(env, get_action, args.len, args.episodes, not(args.norender))
+    env, get_action = load_policy(args.fpath, args.itr if args.itr >= 0 else
+                                  'last', args.deterministic)
+    run_policy(env, get_action, args.len, args.episodes, not (args.norender))
